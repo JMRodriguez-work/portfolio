b16d03439d844deb22ad6487847cb27a9a279ebc
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-03-04 21:51:42 -0300

    UPDATE: sidenav component resume pending

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -26,7 +26,7 @@ const Sidenav = () => {
     <div>
       <AiOutlineMenu
         onClick={handleNav}
-        className="absolute top-4 right-4 z-[99] cursor-pointer md:hidden"
+        className="fixed  top-4 right-4 z-[99] cursor-pointer md:hidden"
       />
       {!!burgerMenu && (
         <div className="flex flex-col fixed w-full h-screen bg-white/90 justify-center items-center z-20">
@@ -51,13 +51,13 @@ const Sidenav = () => {
             <MdWorkOutline size={20} />
             <span className="pl-4">Work</span>
           </a>
-          <a onClick={handleNav}
+          {/* <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#resume"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
-          </a>
+          </a> */}
           <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#contact"
@@ -87,12 +87,12 @@ const Sidenav = () => {
           >
             <MdWorkOutline size={20}/>
           </a>
-          <a
+          {/* <a
             href="#resume"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20}/>
-          </a>
+          </a> */}
           <a
             href="#contact"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-2 cursor-pointer hover:scale-110 ease-in duration-200"
