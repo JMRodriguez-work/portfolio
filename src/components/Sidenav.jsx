c6d5acb2d4c85c18992839b2f9eccc36743ee7e5
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-03-04 21:43:46 -0300

    FIX: bug in Sidenav mobile

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -30,35 +30,35 @@ const Sidenav = () => {
       />
       {!!burgerMenu && (
         <div className="flex flex-col fixed w-full h-screen bg-white/90 justify-center items-center z-20">
-          <a
+          <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#main"
           >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
           </a>
-          <a
+          <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#projects"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
-          <a
+          <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#work"
           >
             <MdWorkOutline size={20} />
             <span className="pl-4">Work</span>
           </a>
-          <a
+          <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#resume"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
-          <a
+          <a onClick={handleNav}
             className="flex justify-center w-[65%] items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
             href="#contact"
           >
