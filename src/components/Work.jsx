2060ced5aa966d0bcae3b13c7cc11fb56159b3d2
Juan Martin Rodriguez <jmartinrodriguez.work@gmail.com>
2023-06-13 16:18:02 -0300

    UPDATE: CV and work experience

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import { WorkItem } from "./index";
 const DATA = [
   {
+    year: 2023,
+    title: "Frontend Developer",
+    description:
+      "I work as a Front End Developer, programming a comprehensive system that empowers clients to manage their customers, reports, beneficiaries, and more. Additionally, I take charge of updating the database for an insurance and brokerage company. My current tech stack includes React for building interactive and efficient interfaces, Sass for managing styles, and MySQL for database management. By leveraging these technologies, I deliver robust and tailored solutions to meet my clients' needs.",
+    duration: "Present",
+  },
+  {
     year: 2021,
     title: "IT Support",
     description:
-      "I manage the Moodle educational platform of the Fray Mamerto Esquiú school. I also maintain the website using WordPress and several administrative tasks",
+      "I manage the Moodle educational platform of the Fray Mamerto Esquiú school. I also maintain the website using WordPress and several administrative tasks. Facilitating training sessions for team members, generating comprehensive system user manuals, and creating engaging video tutorials.",
     duration: "Present",
   },
 ];
